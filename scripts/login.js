e424b86e736c6a0046457fc2418984d5ec21e5c6
Monoton <118214281+MonotonB@users.noreply.github.com>
2023-02-20 09:21:06 +0200

    Added site code.

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+function checkUsernamePassword() {
+    var isLogin = false;
+    var usernameList = ['TestUser1', 'TestUser2'];
+    var passwordList = ["qwe123", "rty456"];
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    for (var i = 0; i < passwordList.length; i++) {
+        if (username == usernameList[i] && password == passwordList[i]) {
+            window.location = "../index.html";
+            alert('Welcome back, ' + username + '!')
+            isLogin = true;
+        }
+        else if (!isLogin) {
+            window.location = "invalid-login.html";
+        }
+    }
+}
